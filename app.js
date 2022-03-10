d4a839749d7b458537053091ed9a85916d9cb18a
charlie alton <charlie_alton123@hotmail.com>
2022-03-09 23:34:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dailyCanvas = document.getElementById("daily-chart");
+
+
 const alertBanner = document.getElementById("alert");
 // create the html for the banner
 alertBanner.innerHTML = <div class="alert-banner">
@@ -49,6 +52,29 @@ let trafficChart = new Chart(trafficCanvas, {
     data: trafficData,
     options: trafficOptions
 });
+
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
 
 
 
