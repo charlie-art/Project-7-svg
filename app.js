ea4e576c3f8291776bf44fb4afae4baa1ef3b5ca
charlie alton <charlie_alton123@hotmail.com>
2022-06-21 13:24:51 -0600

    svg

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
+
+const trafficCanvas = document.getElementById("traffic-chart");
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
         "4-10", "11-17", "18-24", "25-31"],
